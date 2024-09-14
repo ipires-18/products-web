4758916e11b4bbe611b4833a0ec3a6c47b50c8c0
Ian Pires de Oliveira <ian@MacBook-Air-de-Ian.local>
2024-09-14 19:34:39 -0300

    refactor: removed the component main

--- src/components/Main/main.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { Main } from './main'
-
-test('renders Main component with correct text', () => {
-  const { container } = render(<Main />)
-
-  expect(container.firstChild).toHaveStyle({ 'background-color': 'gray' })
-})
--- src/components/Main/main.tsx
+++
@@ -1,6 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-import { MainStyled } from './styles'
-
-export const Main = ({ children }: PropsWithChildren) => {
-  return <MainStyled>{children}</MainStyled>
-}
