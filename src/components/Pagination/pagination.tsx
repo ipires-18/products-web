a84030f4c620ed46645ce036632c7ae26ad4e3de
Ian Pires de Oliveira <ian@MacBook-Air-de-Ian.local>
2024-09-14 19:32:31 -0300

    feat: add component pagination

---
+++ src/components/Pagination/pagination.tsx
@@ -0,0 +1,54 @@
+'use client'
+import React from 'react'
+import * as Styled from './styles'
+
+interface PaginationProps {
+  currentPage: number
+  totalPages: number
+  onPageChange: (page: number) => void
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange
+}) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1)
+    }
+  }
+
+  const handlePageClick = (page: number) => {
+    onPageChange(page)
+  }
+
+  return (
+    <Styled.PaginationContainer>
+      <Styled.PageButton onClick={handlePrevious} disabled={currentPage === 1}>
+        &laquo; Previous
+      </Styled.PageButton>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <Styled.PageNumber
+          key={index + 1}
+          active={currentPage === index + 1}
+          onClick={() => handlePageClick(index + 1)}
+        >
+          {index + 1}
+        </Styled.PageNumber>
+      ))}
+      <Styled.PageButton
+        onClick={handleNext}
+        disabled={currentPage === totalPages}
+      >
+        Next &raquo;
+      </Styled.PageButton>
+    </Styled.PaginationContainer>
+  )
+}
---
+++ src/components/Pagination/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem 0;
+`
+
+export const PageButton = styled.button`
+  background: #828ef9;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  margin: 0 0.25rem;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 1rem;
+  transition: background-color 0.3s;
+  &:disabled {
+    background: #e0e0e0;
+    cursor: not-allowed;
+  }
+  &:hover:not(:disabled) {
+    background: #0056b3;
+  }
+`
+
+export const PageNumber = styled.span<{ active: boolean }>`
+  padding: 0.5rem 1rem;
+  margin: 0 0.25rem;
+  cursor: pointer;
+  border-radius: 5px;
+  background: ${({ active }) => (active ? '#828ef9' : 'transparent')};
+  color: ${({ active }) => (active ? 'white' : '#828ef9')};
+  font-size: 1rem;
+  transition: background-color 0.3s;
+  &:hover {
+    background: #6a4eec;
+    color: white;
+  }
+`
