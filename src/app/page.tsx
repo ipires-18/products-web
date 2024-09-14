175b6ba9825aaf7e87629bf96ba14f3995a1ff82
Ian Pires de Oliveira <ian@MacBook-Air-de-Ian.local>
2024-09-14 19:47:40 -0300

    feat: add page list all products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-'use client'
+import { api } from '@/tools/api'
+import { Card } from '@/components/Card/card'
+import { GridLayout } from '@/components/GridLayout/grid-layout'
+import { ApiResponseProducts } from '@/types/pages/home'
 
-import Button from '@/components/Button'
+async function fetchProducts(): Promise<ApiResponseProducts | null> {
+  try {
+    const response = await api.get('/products?page=1&limit=10')
+    return response.data
+  } catch (error) {
+    console.error('Failed to fetch products:', error)
+    return null
+  }
+}
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetchProducts()
+
   return (
-    <div>
-      <Button title="Clique em mim!" />
-    </div>
+    <>
+      <GridLayout>
+        {response?.products.map((product) => (
+          <Card
+            key={product.id}
+            title={product.title}
+            description={product.description}
+            imageSrc={product.image}
+            price={product.price}
+          />
+        ))}
+      </GridLayout>
+    </>
   )
 }
