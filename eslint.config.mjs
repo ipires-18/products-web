4c685e458541395102d07f701c9cf5baaaa857c9
Ian Pires de Oliveira <ian@MacBook-Air-de-Ian.local>
2024-09-14 12:29:13 -0300

    chore: use the command prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import nextPlugin from '@next/eslint-plugin-next';
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import nextPlugin from '@next/eslint-plugin-next'
 
 export default [
   {
@@ -10,28 +10,31 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: false,
+        warnOnUnsupportedTypeScriptVersion: false
       },
-      sourceType: 'module',
+      sourceType: 'module'
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
       react: reactPlugin,
       'react-hooks': reactHooksPlugin,
-      '@next/next': nextPlugin,
+      '@next/next': nextPlugin
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-      '@next/next/no-img-element': 'off',
+      '@next/next/no-img-element': 'off'
     },
     settings: {
       react: {
-        version: 'detect',
-      },
-    },
-  },
-];
+        version: 'detect'
+      }
+    }
+  }
+]
