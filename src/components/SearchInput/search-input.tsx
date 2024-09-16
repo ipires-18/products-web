9501338c95a128224111bb9500b91329191c04f5
Ian Pires de Oliveira <ian@MacBook-Air-de-Ian.local>
2024-09-15 22:05:38 -0300

    feat: add the componet logic for seach

--- src/components/SearchInput/search-input.tsx
+++ src/components/SearchInput/search-input.tsx
@@ -1,30 +1,26 @@
-'use client'
-import React, { useState } from 'react'
-import { Container, Input } from './styles'
-import { Button } from '../Button/button'
-import { SearchInputProps } from '@/types/components/search-input'
+'use client';
+import React, { useState } from 'react';
+import * as Styled from './styles';
+import { Button } from '@/components/Button/button'
+import { useProducts } from '@/context/ProductsContext';
 
-export const SearchInput: React.FC<SearchInputProps> = ({ onSearch }) => {
-  const [query, setQuery] = useState('')
+export const SearchInput: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const { searchProducts } = useProducts();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value)
-  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
 
   const handleSearch = () => {
-    onSearch(query)
-  }
+    searchProducts(query);
+  };
 
   return (
-    <Container>
-      <Input
-        type="text"
-        value={query}
-        onChange={handleChange}
-        placeholder="Buscar produtos..."
-        aria-label="Buscar produtos"
-      />
-      <Button title="Buscar" onClick={handleSearch} />
-    </Container>
-  )
-}
+    <Styled.Container>
+      <Styled.Input placeholder='Enter a product...' value={query} onChange={handleInputChange} />
+      <Button title='Search' onClick={handleSearch} />
+    </Styled.Container>
+  );
+};
